649b79566ca3088db4ad4b3b4aad768fab25e2fa
Erik Moraru <348i@tuta.io>
2023-02-24 11:21:35 -0400

    feat: add table to `Clinics/Index` page

--- resources/js/Pages/Clinics/Index.vue
+++ resources/js/Pages/Clinics/Index.vue
@@ -6,7 +6,23 @@ defineProps({
 
 <template>
     <h1>Clinics Index</h1>
-    <div v-for="clinic in clinics" :key="clinic.clinicName">
-        {{ clinic.clinicName }}
+    <div class="bg-grey rounded-m shadow overflow-x-auto">
+        <table class="w-full whitespace-nowrap">
+            <thead>
+                <tr class="text-left font-bold">
+                    <th class="pb-4 pt-6 px-6">Clinic ID</th>
+                    <th class="pb-4 pt-6 px-6">Clinic Name</th>
+                    <th class="pb-4 pt-6 px-6">Clinic Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="clinic in clinics" :key="clinic.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+                    <td class="border-t">{{ clinic.clinicID }}</td>
+                    <td class="border-t">{{ clinic.clinicName }}</td>
+                    <td class="border-t">{{ clinic.clinicAddress }}</td>
+                    <td v-if="clinics.creditCardInfo" class="border-t"></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
